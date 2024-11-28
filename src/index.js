e5a27bc5c6e2df3fa1f9789e3b6fd9c147950e0e
Rypex0 <148925881+Rypex0@users.noreply.github.com>
2024-11-27 19:14:23 -0500

    Countdown

--- src/index.js
+++ src/index.js
@@ -2,13 +2,34 @@ const express = require('express');
 const axios = require('axios');
 const moment = require('moment-timezone');
 
-const system = express()
+const system = express();
 
 const PORT = 8888;
 
 const SpreadsheetURL = '';
 
 let CachedItems = [];
+
+function GetRemainingTime() {
+    const CurrentTime = moment.tz("America/New_York");
+    let NextRefresh = moment.tz("America/New_York").endOf('day').set({ hour: 19, minute: 0, second: 0});
+
+    if (CurrentTime.isAfter(NextRefresh)) {
+        NextRefresh.add(1, 'days');
+    }
+
+    const Duration = moment.duration(NextRefresh.diff(CurrentTime));
+    const Hours = Duration.hours().toString().padStart(2, '0');
+    const Minutes = Duration.minutes().toString().padStart(2, '0');
+    const Seconds = Duration.seconds().toString().padStart(2, '0');
+
+    return `${Hours}:${Minutes}:${Seconds}`;
+}
+
+system.get('/countdown', (req, res) => {
+    const RemainingTime = GetRemainingTime();
+    res.json({ time: RemainingTime });
+});
 
 system.use(express.static('src/public'));
 
