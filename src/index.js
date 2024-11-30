8500b5af8efea3869574d2fa736050edda74920b
Rypex0 <148925881+Rypex0@users.noreply.github.com>
2024-11-29 23:04:06 -0500

    UPDATE!

--- src/index.js
+++ src/index.js
@@ -1,14 +1,42 @@
 const express = require('express');
 const axios = require('axios');
 const moment = require('moment-timezone');
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
 
 const system = express();
 
 const PORT = 8888;
 
+const CachedItemsFile = path.join(__dirname, 'CachedItems.json');
+
 const SpreadsheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTM0QfM2QuDPNwfZYL51ct8_YG83I7xEqSOMutEWcIklFK80vix8MqXqHTFt3Z5HBUx8xKbsqgQNgXl/pub?gid=977965621&single=true&output=csv';
 
 let CachedItems = [];
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function LoadItems() {
+    if (fs.existsSync(CachedItemsFile)) {
+        const FileData = fs.readFileSync(CachedItemsFile, 'utf8');
+        if (FileData) {
+            CachedItems = JSON.parse(FileData);
+            console.log('Items Loaded From CachedItems.json.');
+            return true;
+        }
+    }
+    console.log('No Cached Items Found Or CachedItems.json Is Empty.');
+    return false;
+}
+
+function SaveItems(items) {
+    fs.writeFileSync(CachedItemsFile, JSON.stringify(items, null, 2));
+    console.log('Items Saved To CachedItems.json.')
+}
 
 async function FetchItems() {
     try {
@@ -58,17 +86,33 @@ function GetRemainingTime() {
 
 function IsAfter7PM() {
     const CurrentTime = moment.tz("America/New_York");
-    return CurrentTime.hour() >= 19;
+    const ScheduleTime = moment.tz("America/New_York").endOf('day').set({ hour: 19, minute: 0, second: 0 });
+    return CurrentTime.isAfter(ScheduleTime);
 }
 
-async function UpdateItems() {
-    if (IsAfter7PM() && CachedItems.length === 0) {
-        CachedItems = await FetchItems();
+function ScheduleNextRefresh() {
+    const CurrentTime = moment.tz("America/New_York");
+    const ScheduleTime = moment.tz("America/New_York").endOf('day').set({ hour: 19, minute: 0, second: 0 });
+
+    if (CurrentTime.isAfter(ScheduleTime)) {
+        ScheduleTime.add(1, 'day');
     }
+
+    const CTBeforeST = ScheduleTime.diff(CurrentTime);
+
+    console.log(`Next Refresh Scheduled In ${moment.duration(CTBeforeST).humanize()}`);
+    setTimeout(async () => {
+        CachedItems = await FetchItems();
+        SaveItems(CachedItems);
+        ScheduleNextRefresh();
+    }, CTBeforeST);
+}
+
+async function InitializeItems() {
+    LoadItems();
 }
 
 system.get('/item-shop', async (req, res) => {
-    await UpdateItems();
     res.json(CachedItems);
 });
 
@@ -76,6 +120,8 @@ system.get('/countdown', (req, res) => {
     const RemainingTime = GetRemainingTime();
     res.json({ time: RemainingTime });
 });
+
+InitializeItems();
 
 system.use(express.static('src/public'));
 
